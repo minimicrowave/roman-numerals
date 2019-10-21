e947dab2f8d2d209f3bbaaf0486e8be5c2d33bb5
Serene <unexpecte-d@hotmail.com>
2019-10-21 21:41:18 +0800

    Account for simple basic use cases

--- src/RomanNumbers.js
+++ src/RomanNumbers.js
@@ -1,7 +1,13 @@
-
+const ROMAN_ONE = 'I';
 function convertDigits(digit) {
-    return 'hello'
+	if (isNumber(digit)) {
+		return ROMAN_ONE.repeat(digit);
+	}
+	return;
 }
 
+function isNumber(digit) {
+	return typeof digit === 'number';
+}
 
 module.exports = { convertDigits };
--- test/RomanNumbers.spec.js
+++ test/RomanNumbers.spec.js
@@ -3,8 +3,18 @@ const { convertDigits } = require('../src/RomanNumbers');
 
 describe('Roman Numerals', () => {
 	describe('convertDigits()', () => {
-		it('dummy function', () => {
-			expect(convertDigits()).to.equal('hello');
+		it('should return nothing if no input', () => {
+			expect(convertDigits()).to.equal();
+        });
+        
+		it('should return "I" if input = 1', () => {
+			expect(convertDigits(1)).to.equal('I');
+		});
+		it('should return "II" if input = 2', () => {
+			expect(convertDigits(2)).to.equal('II');
+		});
+		it('should return "III" if input = 3', () => {
+			expect(convertDigits(3)).to.equal('III');
 		});
 	});
 });
