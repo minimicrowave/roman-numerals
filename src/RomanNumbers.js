a4aab5be2116ae73756d13be1d1fffea0e481116
Serene <unexpecte-d@hotmail.com>
2019-10-21 23:05:04 +0800

    Account for 4 and 40

--- src/RomanNumbers.js
+++ src/RomanNumbers.js
@@ -6,12 +6,45 @@ const ROMAN_ALPHABETS = {
 	100: 'C'
 };
 
+const DENOMINATORS = {
+	1: 1,
+	5: 1,
+	10: 1,
+	50: 10,
+	100: 10
+};
+
 function convertDigits(digit) {
 	if (isNumber(digit)) {
-		if (ROMAN_ALPHABETS[digit]) return ROMAN_ALPHABETS[digit];
-		return ROMAN_ALPHABETS[1].repeat(digit);
+		return computeRomanAlphabets(digit);
 	}
 	return;
+}
+
+function computeRomanAlphabets(digit) {
+	let computedRomanAlphabet = '';
+	if (ROMAN_ALPHABETS[digit]) return ROMAN_ALPHABETS[digit];
+	let nearestBase = getNearestBase(digit);
+	let nearestDenominator = DENOMINATORS[nearestBase];
+	// console.log(digit, nearestBase, nearestDenominator);
+	if (digit >= nearestBase - nearestDenominator) {
+		computedRomanAlphabet += ROMAN_ALPHABETS[nearestDenominator] + ROMAN_ALPHABETS[nearestBase];
+		// console.log('yeah', digit, computedRomanAlphabet);
+		return computedRomanAlphabet;
+	}
+	return ROMAN_ALPHABETS[1].repeat(digit);
+}
+
+function getNearestBase(digit) {
+	let ROMAN_NUMBER_BASES = Object.keys(ROMAN_ALPHABETS).map(Number);
+	let prevBase = 0;
+
+	for (let i = 0; i < ROMAN_NUMBER_BASES.length; i++) {
+		let currentBase = ROMAN_NUMBER_BASES[i];
+
+		if (digit <= currentBase && digit > prevBase) return currentBase;
+		prevBase = currentBase;
+	}
 }
 
 function isNumber(digit) {
--- test/RomanNumbers.spec.js
+++ test/RomanNumbers.spec.js
@@ -6,7 +6,6 @@ describe('Roman Numerals', () => {
 		it('should return nothing if no input', () => {
 			expect(convertDigits()).to.equal();
         });
-        
 		it('should return "I" if input = 1', () => {
 			expect(convertDigits(1)).to.equal('I');
 		});
@@ -16,11 +15,17 @@ describe('Roman Numerals', () => {
 		it('should return "III" if input = 3', () => {
 			expect(convertDigits(3)).to.equal('III');
 		});
+		it('should return "IV" if input = 4', () => {
+			expect(convertDigits(4)).to.equal('IV');
+		});
 		it('should return "V" if input = 5', () => {
 			expect(convertDigits(5)).to.equal('V');
 		});
 		it('should return "X" if input = 10', () => {
 			expect(convertDigits(10)).to.equal('X');
+        });
+        it('should return "XL" if input = 40', () => {
+			expect(convertDigits(40)).to.equal('XL');
 		});
 		it('should return "L" if input = 50', () => {
 			expect(convertDigits(50)).to.equal('L');
