2c3e2dc5faa0cf06328d96dc28d4e9bf749c290e
Serene <unexpecte-d@hotmail.com>
2019-10-21 21:47:15 +0800

    Add all basic use cases

--- src/RomanNumbers.js
+++ src/RomanNumbers.js
@@ -1,6 +1,15 @@
 const ROMAN_ONE = 'I';
+const ROMAN_FIVE = 'V';
+const ROMAN_TEN = 'X';
+const ROMAN_FIFTY = 'L';
+const ROMAN_HUNDRED = 'C';
+
 function convertDigits(digit) {
 	if (isNumber(digit)) {
+		if (digit === 5) return ROMAN_FIVE;
+		if (digit === 10) return ROMAN_TEN;
+		if (digit === 50) return ROMAN_FIFTY;
+		if (digit === 100) return ROMAN_HUNDRED;
 		return ROMAN_ONE.repeat(digit);
 	}
 	return;
--- test/RomanNumbers.spec.js
+++ test/RomanNumbers.spec.js
@@ -16,5 +16,17 @@ describe('Roman Numerals', () => {
 		it('should return "III" if input = 3', () => {
 			expect(convertDigits(3)).to.equal('III');
 		});
+		it('should return "V" if input = 5', () => {
+			expect(convertDigits(5)).to.equal('V');
+		});
+		it('should return "X" if input = 10', () => {
+			expect(convertDigits(10)).to.equal('X');
+		});
+		it('should return "L" if input = 50', () => {
+			expect(convertDigits(50)).to.equal('L');
+		});
+		it('should return "C" if input = 100', () => {
+			expect(convertDigits(100)).to.equal('C');
+		});
 	});
 });
